aac3ab8e79ffbebc2ff7b182e452c62fca256f30
mayankmandloi <mayankmandloi009@gmail.com>
2018-07-05 17:06:26 +0530

    second test1

--- Day-3/operator.py
+++ Day-3/operator.py
@@ -1,1 +1,8 @@
 #this is a operator
+print(2+3)# 5
+print(2-3)# -1
+print(2*3)# 6
+print(2/3)# 0.6666666....
+print(2%3)# 2
+print(3**3)# 27 3^3
+print(7//3)# floor division decimal ke baad ki value nahi aayegi

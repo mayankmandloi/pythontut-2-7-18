6660ba777020a10c96d605ca92a1bdd91a345bac
mayankmandloi <mayankmandloi009@gmail.com>
2018-07-09 16:52:29 +0530

    Day-5

---
+++ Day-5/assigment.py
@@ -0,0 +1,26 @@
+x=12;
+
+print(x)
+x=14
+print(x)
+x+=6 #x=x+6
+print(x)
+y=1
+z=2
+a=3
+b=4
+c=5
+d=6
+e=7
+y-=1
+print(y)
+z*=2
+print(z)
+a/=2
+print(a)
+c%=2
+print(c)
+b**=2
+print(b)
+d//=3
+print(d)

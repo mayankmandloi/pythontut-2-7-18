5e43864141a341442eaf4d239e5d7c992e389012
mayankmandloi <mayankmandloi009@gmail.com>
2018-07-24 17:01:32 +0530

    Day-13

---
+++ Day-13/sets.py
@@ -0,0 +1,16 @@
+"""
+A set is a collection which is unordered and unindexed. In Python sets are written with curly brackets.
+
+"""
+thisset = {"Student1","Student2","Student 3",1234}
+print(thisset)
+#for stu in thisset:
+#    print("Name:- ",stu)
+
+
+thisset1 = set(("Student1","Student2","Student 3",1234))
+print(thisset1)
+
+thisset.remove("Student2")
+thisset.add("Web Bhumi")
+print(thisset)
---
+++ Day-13/tuple.py
@@ -0,0 +1,15 @@
+"""
+
+A tuple is a collection which is ordered and unchangeable. I
+n Python tuples are written with round brackets.
+"""
+
+thistuple = ("Student1","Student2","Student 3",1234)
+print(thistuple)
+print(thistuple[1])
+
+thistuple1 = tuple(("Student1","Student2","Student 3",1234))
+print(thistuple1)
+print(thistuple1[2])
+#thistuple[1]="Web Bhumi"
+print(thistuple)

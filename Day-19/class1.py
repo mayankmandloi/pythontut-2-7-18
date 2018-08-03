fa5d17f4014bbed0cc83a916abe7aaf1900a94bf
mayankmandloi <mayankmandloi009@gmail.com>
2018-08-03 16:57:03 +0530

    Day-19

---
+++ Day-19/class1.py
@@ -0,0 +1,13 @@
+class MyClass:
+    x=10
+
+p1 = MyClass()
+p2 = MyClass()
+print(p1.x)
+print(p2.x)
+p1.y=23
+p1.x=12
+print(p1.x)
+print(p2.x)
+print(p1.y)
+print(p2.y)
---
+++ Day-19/class2.py
@@ -0,0 +1,7 @@
+class MyClass:
+    name="Mayank"
+    def test(self):
+        print("Hello I am ",self.name)
+
+person=MyClass()
+person.test()
